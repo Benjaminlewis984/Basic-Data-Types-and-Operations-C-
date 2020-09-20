1a88179795624afaf020953b1a6749a7059f9b5f
Benjamin Lewis <benjaminlewis984@gmail.com>
2020-09-20 13:54:00 -0700

    Finished showing how unsigned integers have a limit of 4294967295. Stable

--- main.cpp
+++ main.cpp
@@ -1,4 +1,25 @@
-//
-// Created by Benjamin Lewis on 9/20/20.
-//
+/**************************************************************
+* Class: CSC-600-01 # Fall 2020
+* Group Name: Cabinet
+* Name: Basic Data Types and Operations
+* Student ID: 915173797
+* Name: Benjamin Lewis
+*  File: fsdriver3.c
+*
+* **************************************************************/
 
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!" << std::endl;
+//  Unsigned int in C++ is from -4294967296 to 4294967295
+    unsigned int a = 4294967295;
+    unsigned int b = 3;
+    unsigned int c = a + b;
+    std::cout << "a = " << a << std::endl;
+    std::cout << "b = " << b << std::endl;
+    std::cout << "The answer mathematically should be a + b > 0 since both a & b > 0" << std::endl;
+    std::cout << "a + b = " << c << std::endl;
+
+}
