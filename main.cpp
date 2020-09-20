612862fbd9a84c3ee10e22c59ced803b044f16a0
Benjamin Lewis <benjaminlewis984@gmail.com>
2020-09-20 14:12:20 -0700

    Working for part 3 of homework. Shows that two negatives can equal a positive. Stable

--- main.cpp
+++ main.cpp
@@ -11,20 +11,26 @@
 #include <iostream>
 
 unsigned int unsignedIntegerDataType(unsigned int, unsigned int);
-int integerDataType(int, int);
+int integerDataTypePositive(int, int);
+int integerDataTypeNegative(int, int);
 
 int main()
 {
     std::cout << "Hello World!" << std::endl;
-    //  Unsigned int in C++ is from -4294967296 to 4294967295
+    //  Unsigned int's
     unsigned int a = 4294967295;
     unsigned int b = 1;
     unsignedIntegerDataType(a, b);
 
-    //  Unsigned int in C++ is from -4294967296 to 4294967295
+    // int's
     int c = 2147483647;
     int d = 1;
-    integerDataType(c, d);
+    integerDataTypePositive(c, d);
+    c = -2147483648;
+    d = -1;
+    integerDataTypeNegative(c, d);
+
+
 }
 //  Unsigned int in C++ is from -4294967296 to 4294967295
 unsigned int unsignedIntegerDataType (unsigned int a, unsigned int b)
@@ -32,16 +38,25 @@ unsigned int unsignedIntegerDataType (unsigned int a, unsigned int b)
     unsigned int c = a + b;
     std::cout << "unsigned int a = " << a << std::endl;
     std::cout << "unsigned int b = " << b << std::endl;
-    std::cout << "The answer mathematically should be a + b > 0 since both a & b > 0" << std::endl;
+    std::cout << "The answer mathematically should be a + b > 0 since both a && b > 0" << std::endl;
     std::cout << "a + b = " << c << std::endl;
 }
 
 //  Unsigned int in C++ is from -2147483648 to 2147483647
-int integerDataType (int a, int b)
+int integerDataTypePositive (int a, int b)
 {
     int c = a + b;
     std::cout << "\nint a = " << a << std::endl;
     std::cout << "int b = " << b << std::endl;
-    std::cout << "The answer mathematically should be a + b > 0 since both a & b > 0" << std::endl;
+    std::cout << "The answer mathematically should be a + b > 0 since both a && b > 0" << std::endl;
+    std::cout << "a + b = " << c << std::endl;
+}
+
+int integerDataTypeNegative (int a, int b)
+{
+    int c = a + b;
+    std::cout << "\nint a = " << a << std::endl;
+    std::cout << "int b = " << b << std::endl;
+    std::cout << "The answer mathematically should be a + b < 0 since both a && b < 0" << std::endl;
     std::cout << "a + b = " << c << std::endl;
 }
